81c3695e94bcb48c0b91f8e63a182845ed72a4f2
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-03 11:24:32 -0600

    📅  | Sun Apr  3 11:24:32 MDT 2022

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DrawerComponent } from './drawer/drawer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DrawerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
